0a925b29bbdc21e5d1acd6a49a788477f854f558
Matt Sich <matt.sich@3pillarglobal.com>
2016-11-04 09:47:09 -0400

    added correct fibers version

--- scripts/rh-build.sh
+++ scripts/rh-build.sh
@@ -7,6 +7,7 @@ meteor build bundle --architecture os.linux.x86_64
 cd bundle/
 for file in *.tar.gz; do tar -zxf $file; done
 cd bundle/programs/server/
+npm install fibers@1.0.8 --save
 npm install
 tar -zcvf ../../../node_modules.tar.gz node_modules
 cd ../../../
